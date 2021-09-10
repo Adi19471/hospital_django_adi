86e9ceb7020e5dd402fb929d38279b9967f7d7cd
Adi19471 <akumatha@gmail.com>
2021-09-10 20:33:04 +0530

    static

--- HealthInsurenceCorporence/settings.py
+++ HealthInsurenceCorporence/settings.py
@@ -139,15 +139,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+
+
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+STATIC_ROOT = '/assets/'
 
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
 
 MEDIA_URL ='/media/'
 
