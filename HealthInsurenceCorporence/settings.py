c6caf4373b3408804b5cc3fedc04b7e231773e49
Adi19471 <akumatha@gmail.com>
2021-09-10 22:30:46 +0530

    staticss

--- HealthInsurenceCorporence/settings.py
+++ HealthInsurenceCorporence/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'HealthInsurenceCorporence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': (os.path.join(BASE_DIR, "db.sqlite3")),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': (os.path.join(BASE_DIR, "db.sqlite3")),
+    }
+}
 
 
 
