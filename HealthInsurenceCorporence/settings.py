6ced008a6347f5428bdba1b879d5e789f7aa22d1
Adi19471 <akumatha@gmail.com>
2021-09-10 22:24:07 +0530

    static

--- HealthInsurenceCorporence/settings.py
+++ HealthInsurenceCorporence/settings.py
@@ -144,18 +144,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#      os.path.join(BASE_DIR, 'static'),
+# )
+
+
+
+# MEDIA_URL ='/media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-)
-
-
-
-MEDIA_URL ='/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-
 
 # STATIC_URL = '/static/'
 
@@ -166,9 +167,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # STATIC_ROOT = os.path.join(BASE_DIR,'/assets/')
 
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
 
 CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
